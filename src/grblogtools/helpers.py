3d7833cd1e76c8f87b1cf9c4cff6de759d0fb119
Simon Bowly <simon.bowly@gmail.com>
2021-10-02 23:45:57 +1000

    add categorical parameters

--- src/grblogtools/helpers.py
+++ src/grblogtools/helpers.py
@@ -44,3 +44,49 @@ def fill_default_parameters(summary):
     return summary.groupby("Version").apply(
         partial(fill_for_version, parameter_columns=parameter_columns)
     )
+
+
+parameter_descriptions = {
+    "Method": {
+        -1: "-1: Default",
+        0: "0: Primal Simplex",
+        1: "1: Dual Simplex",
+        2: "2: Barrier",
+        3: "3: Nondeterministic Concurrent",
+        4: "4: Det. Concurrent",
+        5: "5: Det. Conc. Simplex",
+    },
+    "DegenMoves": {
+        -1: "-1: Automatic",
+        0: "0: Disabled",
+    },
+    "Presolve": {
+        -1: "-1: Automatic",
+        0: "0: Off",
+        1: "1: Conservative",
+        2: "2: Aggressive",
+    },
+}
+
+
+def add_categorical_descriptions(summary):
+    """Adds extra columns with informative labels for some columns.
+    This is useful for things like 'method' where treating it as a number means
+    (a) the plot is hard to read, and (b) there are unnecessary gaps in the plot
+    if you use e.g. method = 1, 3, 5. Thus better to have a categorical column
+    to play with here. Downside is the order is not guaranteed..."""
+    parameters = [
+        (parameter, column)
+        for column in summary.columns
+        if (match := re_parameter_column.match(column))
+        and (parameter := match.group(1)) in parameter_descriptions
+    ]
+    for parameter, column in parameters:
+        summary = summary.assign(
+            **{
+                parameter: lambda df: (
+                    df[column].map(parameter_descriptions[parameter]).astype("category")
+                )
+            }
+        )
+    return summary
