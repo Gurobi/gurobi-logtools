429279d360a8b0298f919305e0a5dc64d952d88e
Simon Bowly <bowly@gurobi.com>
2021-12-03 12:01:09 +1100

    Factor CLI components out to separate module

--- src/grblogtools/__main__.py
+++ src/grblogtools/__main__.py
@@ -1,37 +1,7 @@
-import argparse
-import grblogtools as glt
-import pandas as pd
+from .cli import cli
+from . import get_dataframe, __version__
 
-parser = argparse.ArgumentParser(
-    description="grblogtools " + glt.__version__, prog="grblogtools"
+cli(
+    get_dataframe=get_dataframe,
+    argparse_kwargs=dict(description="grblogtools " + __version__, prog="grblogtools"),
 )
-parser.add_argument("outfile", help="Output file name (.xlsx)", metavar="XLSXFILE")
-parser.add_argument("logfiles", help="Gurobi log files", nargs="+", metavar="LOGFILE")
-parser.add_argument(
-    "-t",
-    "--timelines",
-    action="store_true",
-    help="also store timelines (root LP, node log, and NoRel log) in separate sheets",
-)
-parser.add_argument(
-    "-m",
-    "--mergedlogs",
-    action="store_true",
-    help="log files contain multiple logs to be extracted",
-)
-args = parser.parse_args()
-
-if args.timelines:
-    summary, timelines = glt.get_dataframe(
-        args.logfiles, timelines=True, merged_logs=args.mergedlogs
-    )
-    with pd.ExcelWriter(args.outfile) as writer:
-        summary.to_excel(writer, sheet_name="Summary")
-        for tl in timelines:
-            timelines[tl].to_excel(writer, sheet_name=tl)
-else:
-    summary = glt.get_dataframe(args.logfiles, merged_logs=args.mergedlogs)
-    with pd.ExcelWriter(args.outfile) as writer:
-        summary.to_excel(writer, sheet_name="Summary")
-
-print(f"extracted {len(summary)} log(s) to {args.outfile}")
---
+++ src/grblogtools/cli.py
@@ -0,0 +1,37 @@
+import argparse
+import pandas as pd
+
+
+def cli(get_dataframe, argparse_kwargs):
+
+    parser = argparse.ArgumentParser(**argparse_kwargs)
+    parser.add_argument("outfile", help="Output file name (.xlsx)", metavar="XLSXFILE")
+    parser.add_argument("logfiles", help="Gurobi log files", nargs="+", metavar="LOGFILE")
+    parser.add_argument(
+        "-t",
+        "--timelines",
+        action="store_true",
+        help="also store timelines (root LP, node log, and NoRel log) in separate sheets",
+    )
+    parser.add_argument(
+        "-m",
+        "--mergedlogs",
+        action="store_true",
+        help="log files contain multiple logs to be extracted",
+    )
+    args = parser.parse_args()
+
+    if args.timelines:
+        summary, timelines = get_dataframe(
+            args.logfiles, timelines=True, merged_logs=args.mergedlogs
+        )
+        with pd.ExcelWriter(args.outfile) as writer:
+            summary.to_excel(writer, sheet_name="Summary")
+            for tl in timelines:
+                timelines[tl].to_excel(writer, sheet_name=tl)
+    else:
+        summary = get_dataframe(args.logfiles, merged_logs=args.mergedlogs)
+        with pd.ExcelWriter(args.outfile) as writer:
+            summary.to_excel(writer, sheet_name="Summary")
+
+    print(f"extracted {len(summary)} log(s) to {args.outfile}")
