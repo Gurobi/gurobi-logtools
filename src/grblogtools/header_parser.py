55a65132cf4de048a60d2514db769fcf2e21b92f
Maliheh Aramon <maliheha@gmail.com>
2021-12-20 02:31:20 -0800

    clean and self-review

--- src/grblogtools/header_parser.py
+++ src/grblogtools/header_parser.py
@@ -4,8 +4,8 @@ from grblogtools.helpers import ParserFlag
 
 
 class HeaderLogParser:
-    # Each can represent the start line for the header depending on how Gurobi
-    # is ran
+    # Each might represent the start of the header parser depending on how Gurobi
+    # is run
     header_log_starts = [
         re.compile(
             "Gurobi (?P<Version>\d{1,2}\.[^\s]+) \((?P<Platform>[^\)]+)\) logging started (?P<Time>.*)$"
@@ -20,14 +20,14 @@ class HeaderLogParser:
     def __init__(self):
         """Initialize the log parser for the header.
 
-        It is assumed that all the heard log is encapsulated in one line.
+        Note: All header log is captured in one line.
         """
-        # The integer value representing the current flag status
+        # The integer value representing the current parser status
         self.flag = ParserFlag.UNKNOWN.value
         self._log = {}
 
-    def get_flag(self, line: str) -> ParserFlag:
-        """Return the current parsing status of the header log.
+    def get_flag(self, line: str) -> str:
+        """Return the current status of the header parser.
 
         Args:
             line (str): A line in the log file.
@@ -37,19 +37,19 @@ class HeaderLogParser:
         """
         if self.flag == 0:
             for possible_start in HeaderLogParser.header_log_starts:
-                match = possible_start.match(line)
-                if match:
-                    # Populate the log as
+                if match := possible_start.match(line):
+                    # Populate the log as the header log is captured on the start line
                     self._log = {
                         sub_match: value
                         for sub_match, value in match.groupdict().items()
                     }
+                    # Change the status to START
                     self.flag = 1
                     break
         return ParserFlag(self.flag).name
 
     def set_flag(self, status: str):
-        """Set the current parsing status to the given status value."""
+        """Set the current parsing status to the given status name."""
         self.flag = ParserFlag[status].value
 
     def parse(self, line: str) -> str:
@@ -61,7 +61,3 @@ class HeaderLogParser:
     def get_log(self) -> dict:
         """Return the current parsed log."""
         return self._log
-
-
-if __name__ == "__main__":
-    hearder = HeaderLogParser()
