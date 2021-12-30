dd2eff2b2b3ac235d50edb4c8765703da849663f
Maliheh Aramon <maliheha@gmail.com>
2021-12-30 13:58:23 -0800

    change get_log to get_summary and extend the # of lines the header is responsible for

--- src/grblogtools/header_parser.py
+++ src/grblogtools/header_parser.py
@@ -14,14 +14,47 @@ class HeaderParser:
             "Gurobi Compute Server Worker version (?P<Version>\d{1,2}\.[^\s]+) build (.*) \((?P<Platform>[^\)]+)\)$"
         ),
         re.compile("Compute Server job ID: (?P<JobID>.*)$"),
+        re.compile("Set parameter (?P<ParamName>[^\s]+) to value (?P<ParamValue>.*)$"),
+        re.compile("Gurobi Optimizer version (?P<Version>\d{1,2}\.[^\s]+)"),
+    ]
+
+    # Possible intermediate patterns to be parsed
+    header_intermediate_patterns = [
+        re.compile("Set parameter (?P<ParamName>[^\s]+) to value (?P<ParamValue>.*)$"),
+        re.compile(
+            "Gurobi Compute Server Worker version (?P<Version>\d{1,2}\.[^\s]+) build (.*) \((?P<Platform>[^\)]+)\)$"
+        ),
+        re.compile("Compute Server job ID: (?P<JobID>.*)$"),
+        re.compile("Gurobi Optimizer version (?P<Version>\d{1,2}\.[^\s]+)"),
+        re.compile("Read (MPS|LP) format model from file (?P<ModelFilePath>.*)$"),
+        re.compile("Reading time = (?P<ReadingTime>[\d\.]+) seconds"),
+        re.compile(
+            "Thread count was (?P<Threads>\d+) \(of (?P<Cores>\d+) available processors\)"
+        ),
+        re.compile(
+            "Thread count: (?P<PhysicalCores>\d+) physical cores, (?P<LogicalProcessors>\d+) logical processors, using up to (?P<Threads>\d+) threads"
+        ),
     ]
 
     def __init__(self):
-        """Initialize the Header parser.
+        """Initialize the Header parser."""
+        self._summary = {}
 
-        The HeaderParser only includes one line.
-        """
-        self._log = {}
+    def _update_summary(self, match):
+        dict_ = match.groupdict()
+        if "ParamName" in dict_:
+            # Add the keyword "Param" at the beginning of the ParamName to make
+            # it clear it is a parameter
+            self._summary.update(
+                {"Param" + dict_["ParamName"]: convert_data_types(dict_["ParamValue"])}
+            )
+        else:
+            self._summary.update(
+                {
+                    sub_match: convert_data_types(value)
+                    for sub_match, value in dict_.items()
+                }
+            )
 
     def start_parsing(self, line: str) -> bool:
         """Return True if the parser should start parsing the future log lines.
@@ -36,13 +69,8 @@ class HeaderParser:
         for possible_start in HeaderParser.header_start_patterns:
             match = possible_start.match(line)
             if match:
-                # The start line encodes information that should to be stored
-                self._log.update(
-                    {
-                        sub_match: convert_data_types(value)
-                        for sub_match, value in match.groupdict().items()
-                    }
-                )
+                # The start line encodes information that should be stored
+                self._update_summary(match)
                 return True
         return False
 
@@ -55,11 +83,19 @@ class HeaderParser:
         Returns:
             bool: Return True.
         """
+        if not line.strip():
+            return True
+
+        for pattern in HeaderParser.header_intermediate_patterns:
+            match = pattern.match(line)
+            if match:
+                self._update_summary(match)
+                break
         return True
 
-    def get_log(self) -> dict:
-        """Return the current parsed log.
+    def get_summary(self) -> dict:
+        """Return the current parsed summary.
 
         It returns an empty dictionary if the parser is not initialized yet.
         """
-        return self._log
+        return self._summary
