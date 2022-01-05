eee7df02d2bc7a3061781cdef8133045dab6e9aa
Simon Bowly <bowly@gurobi.com>
2022-01-05 23:22:22 +1100

    Add basic termination parser

---
+++ src/grblogtools/termination.py
@@ -0,0 +1,45 @@
+import re
+
+from grblogtools.helpers import typeconvert_groupdict
+
+
+class TerminationParser:
+
+    patterns = [
+        re.compile(r"Solution count (?P<SolCount>\d+)"),
+        re.compile(
+            r"Best objective (?P<ObjVal>[^,]+), best bound (?P<ObjBound>[^,]+), gap (?P<MIPGap>.*)$"
+        ),
+    ]
+
+    re_termination_status = {
+        "OPTIMAL": re.compile(
+            r"(?P<OPTIMAL>Optimal solution found)(?: \(tolerance .*\))"
+        ),
+    }
+
+    def __init__(self):
+        self._summary = {}
+
+    def _parse(self, line: str) -> bool:
+        """No specific section start, so start and continue are the same."""
+        for pattern in self.patterns:
+            match = pattern.match(line)
+            if match:
+                self._summary.update(typeconvert_groupdict(match))
+                return True
+        for status, pattern in self.re_termination_status.items():
+            match = pattern.match(line)
+            if match:
+                self._summary["Status"] = status
+                return True
+        return False
+
+    def start_parsing(self, line: str) -> bool:
+        return self._parse(line)
+
+    def continue_parsing(self, line: str) -> bool:
+        return self._parse(line)
+
+    def get_summary(self) -> dict:
+        return self._summary
---
+++ tests/test_termination.py
@@ -0,0 +1,20 @@
+from grblogtools.helpers import parse_block
+from grblogtools.termination import TerminationParser
+
+test_lines_1 = """
+Solution count 10: 1.20001e+09 1.26668e+09 1.40001e+09 ... 1.50001e+09
+Optimal solution found (tolerance 1.00e-04)
+Best objective 1.200012600000e+09, best bound 1.200012600000e+09, gap 0.0000%
+"""
+
+
+def test_termination_parser():
+    parser = TerminationParser()
+    parse_block(parser, test_lines_1)
+    assert parser.get_summary() == {
+        "SolCount": 10,
+        "ObjVal": 1.2000126e09,
+        "ObjBound": 1.2000126e09,
+        "MIPGap": 0.0,
+        "Status": "OPTIMAL",
+    }
