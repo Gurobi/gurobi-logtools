1038c7f1b637e8a4cb1dc0c63644b16a18c9480b
Maliheh Aramon <maliheha@users.noreply.github.com>
2022-01-20 13:58:25 -0800

    pre-commit-workflow-(missing part) (#32)
    * add dummy file
    
    * add pre-commit env
    
    * fix the formatting

--- tests/helpers.py
+++ tests/helpers.py
@@ -1,6 +1,7 @@
 import grblogtools as glt
 
+
 def read_single(filename):
-    summary = glt.get_dataframe([f'data/{filename}'])
-    rows = summary.to_dict(orient='records')
+    summary = glt.get_dataframe([f"data/{filename}"])
+    rows = summary.to_dict(orient="records")
     return rows[0]
