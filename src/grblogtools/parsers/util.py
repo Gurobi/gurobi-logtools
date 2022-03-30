77a9207fe9d7cb54f9ed3aabb5b655eb484d17d0
Simon Bowly <bowly@gurobi.com>
2022-03-30 14:57:48 +1100

    Fix an issue with interrupt terminations
    Termination regexes could return None values, which then
    failed in type conversion.

--- src/grblogtools/parsers/util.py
+++ src/grblogtools/parsers/util.py
@@ -10,7 +10,7 @@ date_time_regex = re.compile(r"\D+\s\D+\s\d+\s\d+:\d+:\d+\s\d{4}")
 
 def convert_data_types(value):
     """Convert the given value string to the type it matches."""
-    if value == "-":
+    if value is None or value == "-":
         # Commonly used sentinel for a missing value in log tables
         return None
     elif int_regex.match(value):
--- tests/parsers/test_termination.py
+++ tests/parsers/test_termination.py
@@ -1,6 +1,7 @@
 from unittest import TestCase, main
 
 from grblogtools.parsers.termination import TerminationParser
+from grblogtools.parsers.util import parse_block
 
 example_log = """
 Barrier solved model in 17 iterations and 4.83 seconds (6.45 work units)
@@ -25,6 +26,25 @@ expected_summary = {
 }
 
 
+interrupted_log = """
+Explored 3818 nodes (2594430 simplex iterations) in 9431.94 seconds
+Thread count was 8 (of 8 available processors)
+
+Solution count 4: -1.72151e+08 -1.71457e+08 -1.70301e+08 -1.49849e+08
+
+Solve interrupted
+Best objective -1.721508552690e+08, best bound -1.763085601859e+08, gap 2.4152%
+"""
+
+interrupted_summary = {
+    "Status": "INTERRUPTED",
+    "SolCount": 4,
+    "Threads": 8,
+    "Cores": 8,
+    "ErrorCode": None,
+}
+
+
 class TestTermination(TestCase):
     def setUp(self):
         pass
@@ -35,6 +55,11 @@ class TestTermination(TestCase):
         for line in lines:
             parser.parse(line)
         self.assertEqual(parser.get_summary(), expected_summary)
+
+    def test_interrupted(self):
+        parser = TerminationParser()
+        parse_block(parser, interrupted_log)
+        self.assertEqual(parser.get_summary(), interrupted_summary)
 
 
 if __name__ == "__main__":
