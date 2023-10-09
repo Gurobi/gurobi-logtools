b9294ad6623a989fe4c1cf95771ce50c7f5b1275
Matthias Miltenberger <miltenberger@gurobi.com>
2023-10-09 12:34:55 +0200

    rename project to gurobi-logtools

--- src/gurobi_logtools/__main__.py
+++ src/gurobi_logtools/__main__.py
@@ -3,5 +3,7 @@ from gurobi_logtools.cli import cli
 
 cli(
     glt_parse=parse,
-    argparse_kwargs=dict(description="gurobi-logtools " + __version__, prog="gurobi_logtools"),
+    argparse_kwargs=dict(
+        description="gurobi-logtools " + __version__, prog="gurobi_logtools"
+    ),
 )
