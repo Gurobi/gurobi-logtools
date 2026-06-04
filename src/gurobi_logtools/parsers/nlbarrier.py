e6414e38ecda80946260a7ea6827b3eb93c64c45
venaturum <venaturum@gmail.com>
2026-06-04 23:20:49 +1000

    parsing for nlbarrier

--- src/gurobi_logtools/parsers/nlbarrier.py
+++ src/gurobi_logtools/parsers/nlbarrier.py
@@ -9,35 +9,29 @@ from gurobi_logtools.parsers.util import (
 )
 
 
-class BarrierParser(Parser):
+class NonLinearBarrierParser(Parser):
     # The pattern indicating the initialization of the parser
-    barrier_start_pattern = re.compile(
-        r"Iter(\s+)Primal(\s+)Dual(\s+)Primal(\s+)Dual(\s+)Compl(\s+)Time",
+    nlbarrier_start_pattern = re.compile(
+        r"Iter(\s+)Objective(\s+)Primal(\s+)Dual(\s+)Compl(\s+)Step(\s+)Time",
     )
 
     # The pattern indicating the barrier progress
-    barrier_progress_pattern = re.compile(
-        r"\s*(?P<Iteration>\d+)(?P<Indicator>\s|\*)\s+(?P<PObj>[^\s]+)\s+(?P<DObj>[^\s]+)\s+(?P<PRes>[^\s]+)\s+(?P<DRes>[^\s]+)\s+(?P<Compl>[^\s]+)\s+(?P<Time>\d+)s",
+    nlbarrier_progress_pattern = re.compile(
+        r"\s*(?P<Iteration>\d+)(?P<Indicator>\s|\*)\s+(?P<Objective>[^\s]+)\s+(?P<PRes>[^\s]+)\s+(?P<DRes>[^\s]+)\s+(?P<Compl>[^\s]+)\s+(?P<Step>[^\s]+)\s+(?P<Time>\d+)s",
     )
 
     # The pattern indicating the termination of the barrier algorithm
-    barrier_termination_patterns = [
+    nlbarrier_termination_patterns = [
         re.compile(
-            rf"Barrier solved model in (?P<BarIterCount>[^\s]+) iterations and (?P<Runtime>{float_pattern}) seconds \((?P<Work>{float_pattern}) work units\)",
+            rf"NL barrier solved model in (?P<NLBarIterCount>[^\s]+) iterations and (?P<Runtime>{float_pattern}) seconds \((?P<Work>{float_pattern}) work units\)",
         ),
         re.compile(
-            rf"Barrier performed (?P<BarIterCount>\d+) iterations in (?P<Runtime>{float_pattern}) seconds \((?P<Work>{float_pattern}) work units\)",
-        ),
-        re.compile(
-            r"Barrier solved model in (?P<BarIterCount>[^\s]+) iterations and (?P<Runtime>[^\s]+) seconds",
-        ),
-        re.compile(
-            r"Barrier performed (?P<BarIterCount>\d+) iterations in (?P<Runtime>[^\s]+) seconds",
+            rf"NL barrier performed (?P<NLBarIterCount>\d+) iterations in (?P<Runtime>{float_pattern}) seconds \((?P<Work>{float_pattern}) work units\)",
         ),
     ]
 
     def __init__(self):
-        """Initialize the Barrier parser."""
+        """Initialize the Nonlinear Barrier parser."""
         self._summary = {}
         self._progress = []
         self._started = False
@@ -62,23 +56,25 @@ class BarrierParser(Parser):
         """
 
         if not self._started:
-            match = BarrierParser.barrier_start_pattern.match(line)
+            match = NonLinearBarrierParser.nlbarrier_start_pattern.match(line)
             if match:
                 self._started = True
                 return ParseResult(matched=True)
             return ParseResult(matched=False)
 
-        progress_match = BarrierParser.barrier_progress_pattern.match(line)
+        progress_match = NonLinearBarrierParser.nlbarrier_progress_pattern.match(line)
         if progress_match:
-            entry = {"Type": "barrier"}
+            entry = {"Type": "nlbarrier"}
             entry.update(typeconvert_groupdict(progress_match))
             self._progress.append(entry)
             return ParseResult(entry)
 
-        for barrier_termination_pattern in BarrierParser.barrier_termination_patterns:
-            barrier_termination_match = barrier_termination_pattern.match(line)
-            if barrier_termination_match:
-                parse_result = typeconvert_groupdict(barrier_termination_match)
+        for (
+            nlbarrier_termination_pattern
+        ) in NonLinearBarrierParser.nlbarrier_termination_patterns:
+            nlbarrier_termination_match = nlbarrier_termination_pattern.match(line)
+            if nlbarrier_termination_match:
+                parse_result = typeconvert_groupdict(nlbarrier_termination_match)
                 self._handle_repeat_barrier_solve(parse_result)  # modifies parse_result
                 self._summary.update(parse_result)
                 return ParseResult(parse_result)
