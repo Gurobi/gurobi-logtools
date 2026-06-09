bdd6ef5b94136c290593a5ad97bceddab0379696
Venaturum <venaturum@gmail.com>
2026-06-09 20:33:28 +1000

    [GH129] parsing "solution objective" at end of nl barrier (#130)

--- tests/parsers/test_continuous.py
+++ tests/parsers/test_continuous.py
@@ -219,6 +219,96 @@ expected_progress_pdhg = [
     },
 ]
 
+example_log_nlbarrier = """
+Ordering time: 0.05s
+
+NL barrier statistics:
+  Hessian NZ  : 3.702e+04
+  Jacobian NZ : 5.336e+05
+  Factor NZ   : 2.711e+06
+  Factor Ops  : 2.358e+07
+  Threads     : 16
+
+                           Residuals
+Iter     Objective      Primal     Dual      Compl     Step     Time
+   0   4.34213397e+08  8.28e+02  2.00e+03  5.54e+03  0.00e+00     1s
+   1   1.16203011e+09  5.54e+02  1.96e+03  5.43e+03  3.31e-01     2s
+   2   2.55211279e+09  4.42e+02  1.94e+03  5.37e+03  2.02e-01     2s
+   3   6.26087199e+09  3.45e+02  1.79e+03  4.95e+03  2.18e-01     2s
+   4   1.59558942e+10  2.96e+02  1.70e+03  4.66e+03  1.43e-01     2s
+
+NL barrier solved model in 48 iterations and 6.36 seconds (9.02 work units)
+First-order optimal solution
+Solution objective 5.665143444362e+10
+"""
+
+expected_summary_nlbarrier = {
+    "Status": "OPTIMAL",
+    "ObjVal": 56651434443.62,
+    "OrderingTime": 0.05,
+    "NLBarIterCount": 48,
+    "Runtime": 6.36,
+    "Work": 9.02,
+}
+
+expected_progress_nlbarrier = [
+    {
+        "Type": "nlbarrier",
+        "Iteration": 0,
+        "Indicator": " ",
+        "Objective": 434213397.0,
+        "PRes": 828.0,
+        "DRes": 2000.0,
+        "Compl": 5540.0,
+        "Step": 0.0,
+        "Time": 1,
+    },
+    {
+        "Type": "nlbarrier",
+        "Iteration": 1,
+        "Indicator": " ",
+        "Objective": 1162030110.0,
+        "PRes": 554.0,
+        "DRes": 1960.0,
+        "Compl": 5430.0,
+        "Step": 0.331,
+        "Time": 2,
+    },
+    {
+        "Type": "nlbarrier",
+        "Iteration": 2,
+        "Indicator": " ",
+        "Objective": 2552112790.0,
+        "PRes": 442.0,
+        "DRes": 1940.0,
+        "Compl": 5370.0,
+        "Step": 0.202,
+        "Time": 2,
+    },
+    {
+        "Type": "nlbarrier",
+        "Iteration": 3,
+        "Indicator": " ",
+        "Objective": 6260871990.0,
+        "PRes": 345.0,
+        "DRes": 1790.0,
+        "Compl": 4950.0,
+        "Step": 0.218,
+        "Time": 2,
+    },
+    {
+        "Type": "nlbarrier",
+        "Iteration": 4,
+        "Indicator": " ",
+        "Objective": 15955894200.0,
+        "PRes": 296.0,
+        "DRes": 1700.0,
+        "Compl": 4660.0,
+        "Step": 0.143,
+        "Time": 2,
+    },
+]
+
 
 class TestContinuous(TestCase):
     def test_last_progress_entry_barrier_with_simplex(self):
@@ -236,12 +326,14 @@ class TestContinuous(TestCase):
                 example_log_relaxation,
                 example_log_mip,
                 example_log_pdhg,
+                example_log_nlbarrier,
             ],
             [
                 expected_summary_concurrent,
                 expected_summary_relaxation,
                 expected_summary_mip,
                 expected_summary_pdhg,
+                expected_summary_nlbarrier,
             ],
             [
                 expected_progress_concurrent,
